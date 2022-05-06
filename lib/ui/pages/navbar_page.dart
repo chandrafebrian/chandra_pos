d6536687c2d9cca45ac72977d7d72f9c8f2fb568
chandrafebrian <chandrafebrian99@gmail.com>
2022-05-06 18:38:19 +0700

    up
    Signed-off-by: chandrafebrian <chandrafebrian99@gmail.com>

--- lib/ui/pages/navbar_page.dart
+++ lib/ui/pages/navbar_page.dart
@@ -10,60 +10,43 @@ class NavBarPage extends StatefulWidget {
 class _NavBarPageState extends State<NavBarPage> {
   @override
   Widget build(BuildContext context) {
-    return Drawer(child:
-        BlocBuilder<PenggunaBloc, PenggunaState>(builder: (context, state) {
-      if (state is PenggunaLoading) {
-        if (imageFileToUpload != null) {
-          uploadImage(imageFileToUpload!).then((value) {
-            imageFileToUpload = null;
-
-            context.read<PenggunaBloc>().add(UpdateDataPengguna(
-                  profilePicture: value,
-                ));
-          });
-        }
-        return ListView(
-          padding: EdgeInsets.zero,
+    return Drawer(
+      child: Container(
+        color: const Color.fromARGB(255, 55, 15, 157),
+        child: ListView(
           children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: Image(
-                width: 100,
-                height: 100,
-                image: (state.pengguna.profilePicture == ''
-                    ? const AssetImage(
-                        'assets/user_pic.png',
-                      ) as ImageProvider
-                    : NetworkImage(state.pengguna.profilePicture!)),
-                fit: BoxFit.cover,
-              ),
-              decoration: const BoxDecoration(
-                color: Colors.deepPurple,
-              ),
-              accountName: Text(
-                state.pengguna.namaOutlet!,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 15,
-                ),
-              ),
-              accountEmail: Text(
-                state.pengguna.email!,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 15,
-                ),
-              ),
+            const SizedBox(
+              height: 20,
             ),
-            ListTile(),
+            ListTile(
+              leading:
+                  const Icon(Icons.shopping_bag_outlined, color: Colors.white),
+              title: const Text('POINT OF SALE',
+                  style: TextStyle(color: Colors.white)),
+              onTap: () {
+                // untuk pindah halaman
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.punch_clock, color: Colors.white),
+              title:
+                  const Text('ACTIVITY', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                // untuk pindah halaman
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings, color: Colors.white),
+              title:
+                  const Text('SETTING', style: TextStyle(color: Colors.white)),
+              onTap: () {
+                // untuk pindah halaman
+              },
+            ),
           ],
-        );
-      } else {
-        return const SpinKitFadingCircle(
-          color: Colors.deepPurple,
-          size: 50,
-        );
-      }
-    }));
+        ),
+      ),
+    );
   }
 }
 // 
@@ -71,3 +54,60 @@ class _NavBarPageState extends State<NavBarPage> {
 
 
 
+
+
+// 
+// 
+// return Drawer(child:
+//         BlocBuilder<PenggunaBloc, PenggunaState>(builder: (context, state) {
+//       if (state is PenggunaLoading) {
+//         if (imageFileToUpload != null) {
+//           uploadImage(imageFileToUpload!).then((value) {
+//             imageFileToUpload = null;
+
+//             context.read<PenggunaBloc>().add(UpdateDataPengguna(
+//                   profilePicture: value,
+//                 ));
+//           });
+//         }
+//         return ListView(
+//           padding: EdgeInsets.zero,
+//           children: [
+//             UserAccountsDrawerHeader(
+//               currentAccountPicture: Image(
+//                 width: 100,
+//                 height: 100,
+//                 image: (state.pengguna.profilePicture == ''
+//                     ? const AssetImage(
+//                         'assets/user_pic.png',
+//                       ) as ImageProvider
+//                     : NetworkImage(state.pengguna.profilePicture!)),
+//                 fit: BoxFit.cover,
+//               ),
+//               decoration: const BoxDecoration(
+//                 color: Colors.deepPurple,
+//               ),
+//               accountName: Text(
+//                 state.pengguna.namaOutlet!,
+//                 style: const TextStyle(
+//                   color: Colors.white,
+//                   fontSize: 15,
+//                 ),
+//               ),
+//               accountEmail: Text(
+//                 state.pengguna.email!,
+//                 style: const TextStyle(
+//                   color: Colors.white,
+//                   fontSize: 15,
+//                 ),
+//               ),
+//             ),
+//           ],
+//         );
+//       } else {
+//         return const SpinKitFadingCircle(
+//           color: Colors.deepPurple,
+//           size: 50,
+//         );
+//       }
+//     }));
