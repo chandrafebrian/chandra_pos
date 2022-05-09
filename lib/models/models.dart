468a2f86938f02697d4ce8b862e77e19b23676d0
chandrafebrian <chandrafebrian99@gmail.com>
2022-05-09 23:37:25 +0700

    up
    Signed-off-by: chandrafebrian <chandrafebrian99@gmail.com>

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -2,3 +2,4 @@ import 'dart:io';
 import 'package:equatable/equatable.dart';
 part 'pengguna.dart';
 part 'registration_data.dart';
+part 'model_nilai_meja.dart';
--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 part 'selectable_box.dart';
 part 'selected_nav_page.dart';
+part 'widget_meja.dart';
