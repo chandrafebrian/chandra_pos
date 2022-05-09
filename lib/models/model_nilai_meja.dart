44486cf5061211a32e915b07148c8a5bc2eb32ba
chandrafebrian <chandrafebrian99@gmail.com>
2022-05-09 23:48:40 +0700

    up
    Signed-off-by: chandrafebrian <chandrafebrian99@gmail.com>

--- lib/models/model_nilai_meja.dart
+++ lib/models/model_nilai_meja.dart
@@ -1,1 +1,16 @@
 part of 'models.dart';
+
+class ModelNilaiMeja extends Equatable {
+  final String id;
+  final String namaMeja;
+  final int nomorMeja;
+
+  const ModelNilaiMeja({
+    required this.id,
+    required this.namaMeja,
+    required this.nomorMeja,
+  });
+
+  @override
+  List<Object?> get props => throw UnimplementedError();
+}
