8b8cac87b57408257cfdfbbb18c6e47f5dfbc562
chandrafebrian <chandrafebrian99@gmail.com>
2022-06-01 18:53:32 +0700

    up
    Signed-off-by: chandrafebrian <chandrafebrian99@gmail.com>

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -15,6 +15,9 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController namaMenuController = TextEditingController();
+    TextEditingController hargaMenuController = TextEditingController();
+
     return Scaffold(
       // drawer: const NavBarPage(),
       appBar: AppBar(
@@ -24,29 +27,77 @@ class _MainPageState extends State<MainPage> {
           IconButton(
             icon: const Icon(Icons.add),
             onPressed: () {
-              context
-                  .read<PageBloc>()
-                  .add(GoToHalamanMenu(widget.baruModelMenu as BaruModelMenu));
+              // context.read<PageBloc>().add(GoToHalamanMenu());
               // isi untuk halaman buat menu baru
             },
           ),
           const SizedBox(width: defaultMargin),
         ],
       ),
-      body: SizedBox(
-        height: 30,
+
+      body: Center(
         child: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              AuthServices().signOut();
+          child: BlocBuilder<PenggunaBloc, PenggunaState>(
+            builder: (context, state) {
+              Pengguna pengguna = (state as PenggunaLoading).pengguna;
+              return Scaffold(
+                  body: Column(
+                children: [
+                  TextField(
+                    controller: namaMenuController,
+                    decoration: InputDecoration(
+                      hintText: 'Nama Menu',
+                      hintStyle: GoogleFonts.lato(fontSize: 20),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: hargaMenuController,
+                    decoration: InputDecoration(
+                      hintText: 'Harga Menu',
+                      hintStyle: GoogleFonts.lato(fontSize: 20),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  FloatingActionButton(
+                    child: const Icon(Icons.arrow_forward),
+                    onPressed: () async {
+                      //
+                      if (namaMenuController.text.length < 3 ||
+                          hargaMenuController.text.length < 3) {
+                        Flushbar(
+                          backgroundColor: Colors.pink,
+                          duration: const Duration(milliseconds: 1000),
+                          flushbarPosition: FlushbarPosition.TOP,
+                          message: 'nama kurang dari 3 karakter',
+                        ).show(context);
+                      } else {
+                        await ServicesMenu().saveMenukefirebase(BaruModelMenu(
+                            pengguna.id,
+                            namaMenuController.text,
+                            hargaMenuController.text.length));
+                      }
+                    },
+                  ),
+                ],
+                //
+              ));
             },
-            child: const Text('out'),
           ),
         ),
       ),
     );
   }
 }
+
+
+
+
 
 // code for sign out!
 //  ElevatedButton(
