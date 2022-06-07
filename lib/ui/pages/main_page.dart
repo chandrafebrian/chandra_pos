8ac483873c45fe02b7f8d921137f7bab9f4d97f9
chandrafebrian <chandrafebrian99@gmail.com>
2022-06-07 21:44:24 +0700

    up
    Signed-off-by: chandrafebrian <chandrafebrian99@gmail.com>

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -13,13 +13,13 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  TextEditingController namaMenuController = TextEditingController();
+  TextEditingController hargaMenuController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    TextEditingController namaMenuController = TextEditingController();
-    TextEditingController hargaMenuController = TextEditingController();
-
     return Scaffold(
-      // drawer: const NavBarPage(),
+      drawer: const NavBarPage(),
       appBar: AppBar(
         backgroundColor: warnaUtamaChandraPos,
         title: Text('Point of Sale', style: GoogleFonts.lato(fontSize: 19)),
@@ -34,66 +34,69 @@ class _MainPageState extends State<MainPage> {
           const SizedBox(width: defaultMargin),
         ],
       ),
-
       body: Center(
         child: Center(
-          child: BlocBuilder<PenggunaBloc, PenggunaState>(
-            builder: (context, state) {
-              Pengguna pengguna = (state as PenggunaLoading).pengguna;
-              return Scaffold(
-                  body: Column(
-                children: [
-                  TextField(
-                    controller: namaMenuController,
-                    decoration: InputDecoration(
-                      hintText: 'Nama Menu',
-                      hintStyle: GoogleFonts.lato(fontSize: 20),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
+          child: Scaffold(
+            body: Column(
+              children: [
+                TextField(
+                  controller: namaMenuController,
+                  decoration: InputDecoration(
+                    hintText: 'Nama Menu',
+                    hintStyle: GoogleFonts.lato(fontSize: 20),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
                   ),
-                  const SizedBox(height: 10),
-                  TextField(
-                    controller: hargaMenuController,
-                    decoration: InputDecoration(
-                      hintText: 'Harga Menu',
-                      hintStyle: GoogleFonts.lato(fontSize: 20),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: hargaMenuController,
+                  decoration: InputDecoration(
+                    hintText: 'Harga Menu',
+                    hintStyle: GoogleFonts.lato(fontSize: 20),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
                   ),
-                  FloatingActionButton(
-                    child: const Icon(Icons.arrow_forward),
-                    onPressed: () async {
-                      //
-                      if (namaMenuController.text.length < 3 ||
-                          hargaMenuController.text.length < 3) {
-                        Flushbar(
-                          backgroundColor: Colors.pink,
-                          duration: const Duration(milliseconds: 1000),
-                          flushbarPosition: FlushbarPosition.TOP,
-                          message: 'nama kurang dari 3 karakter',
-                        ).show(context);
-                      } else {
-                        await ServicesMenu().saveMenukefirebase(BaruModelMenu(
-                            pengguna.id,
-                            namaMenuController.text,
-                            hargaMenuController.text.length));
-                      }
-                    },
-                  ),
-                ],
-                //
-              ));
-            },
+                ),
+                FloatingActionButton(
+                  child: const Icon(Icons.arrow_forward),
+                  onPressed: () async {
+                    //
+                    if (namaMenuController.text.length < 3 ||
+                        hargaMenuController.text.length < 3) {
+                      Flushbar(
+                        backgroundColor: Colors.pink,
+                        duration: const Duration(milliseconds: 1000),
+                        flushbarPosition: FlushbarPosition.TOP,
+                        message: 'nama kurang dari 3 karakter',
+                      ).show(context);
+                    } else {
+                      // else {
+                      ServicesMenu();
+                      // }
+                      // context.read<PageBloc>().add(GoToHalamanMenu());
+                      // isi untuk halaman buat menu baru
+                    }
+
+                    // await ServicesMenu().saveMenukefirebase(BaruModelMenu(
+                    //     pengguna.id,
+                    //     namaMenuController.text,
+                    //     hargaMenuController.text.length));
+                  },
+                ),
+              ],
+              //
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+
 
 
 
