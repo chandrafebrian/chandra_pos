b16524c2c3a5e53e5e19583eb7a959dbcb5c3239
chandrafebrian <chandrafebrian99@gmail.com>
2022-07-10 11:12:03 +0700

    up
    Signed-off-by: chandrafebrian <chandrafebrian99@gmail.com>

--- lib/services/services_menu.dart
+++ lib/services/services_menu.dart
@@ -5,7 +5,7 @@ class ServicesMenu {
       FirebaseFirestore.instance.collection('collectMenus');
 
   Future<void> saveMenukefirebase(BaruModelMenu baruModelMenu) async {
-    await menuCollection.doc().set({
+    await menuCollection.add({
       'email': '',
       'namaBarang': baruModelMenu.namaBarang,
       'hargaBarang': baruModelMenu.hargaBarang,
